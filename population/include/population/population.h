227aa6931cde41ef40d808af656bbb1c292102d6
Raihan Kibria <rkibria@users.noreply.github.com>
2021-09-04 23:47:07 +0100

    population

--- population/include/population/population.h
+++ population/include/population/population.h
@@ -15,13 +15,19 @@ public:
     Population();
     ~Population();
 
+    Population(Population const&) = delete;
+    Population& operator=(Population const&) = delete;
+
     size_t size() const;
     Individual* getIndividual(size_t i) const;
     void addIndividual(std::unique_ptr<Individual>&& idv);
 
+    void evolve(double spread);
+    void clearFitnesses();
+
 private:
     std::unique_ptr<PopulationVector> individuals;
-
+    bool isFirstGeneration{ true };
 };
 
 #endif
--- population/src/population.cpp
+++ population/src/population.cpp
@@ -1,9 +1,10 @@
 #include "population/population.h"
 
+#include <algorithm>
+
 Population::Population()
     : individuals{ std::make_unique<PopulationVector>() }
 {
-
 }
 
 Population::~Population()
@@ -23,4 +24,35 @@ size_t Population::size() const
 void Population::addIndividual(std::unique_ptr<Individual>&& idv)
 {
     individuals->emplace_back(std::move(idv));
+}
+
+void Population::evolve(double spread)
+{
+    for(const auto& uptr : *individuals) {
+        uptr->evaluate();
+    }
+
+    std::sort(individuals->begin(), individuals->end(),
+              [](const std::unique_ptr<Individual>& idv1,
+              const std::unique_ptr<Individual>& idv2) { return idv1->getFitness() < idv2->getFitness(); } );
+
+    const auto halfSize = individuals->size() / 2;
+
+    for(size_t i = 0; i < halfSize; ++i) {
+        const auto& parent = (*individuals)[i];
+        const auto& offspring = (*individuals)[halfSize + i];
+
+        *offspring = *parent;
+        offspring->mutate(spread);
+        if(i != 0) {
+            parent->mutate(spread);
+        }
+    }
+}
+
+void Population::clearFitnesses()
+{
+    for(size_t i = 0; i < individuals->size(); ++i) {
+        (*individuals)[i]->setFitness(0);
+    }
 }
