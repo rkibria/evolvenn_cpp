5e33bf23d000ffa33b7ccbd1d8e18dc78d1cf127
Raihan Kibria <rkibria@users.noreply.github.com>
2021-09-04 20:49:55 +0100

    Speed test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
 //#include <vector>
 //#include <cmath>
+#include <chrono>
 
 //#include <tbb/parallel_for.h>
 
 #include "neuralnet/neuralnet.h"
 
+void speedTest1()
+{
+    NeuralNet nn(2, {2, 3, 2});
+    nn.setWeights({   0, 1, 1,
+                      0, 1, 1,
+
+                      0, 1, 1,
+                      0, 1, 1,
+                      0, 1, 1,
+
+                      0, 1, 1, -1,
+                      0, 1, -1, -1
+                  });
+    std::vector<double> outputs;
+    double inputs[2] = {1.0, 2.0};
+
+    const auto start = std::chrono::high_resolution_clock::now();
+
+    for(size_t i = 0; i < 1000000; ++i) {
+        nn.run(inputs, outputs);
+        outputs[0] += 0.00001;
+    }
+
+    const auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    std::cout << "Time 1: " << duration.count() << " ms" << std::endl;
+}
+
 int main(int argc, char **argv)
 {
-    NeuralNet nn(4, {4, 4, 2});
+    speedTest1();
 
     return 0;
 }
