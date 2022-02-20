4486ca173039d535dffa4889a405cf80f450bf77
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2022-02-20 17:16:30 +0000

    Draw best of generations

--- main.cpp
+++ main.cpp
@@ -96,32 +96,25 @@ void evolution1()
 {
     HtmlAnim::HtmlAnim anim("sinus evolution progress");
     const int outW = 500, outH = 300;
-    anim.frame().save().stroke_style("gray")
-        .line(0,outH/2, outW,outH/2)
-        .line(outW/2,0, outW/2,outH);
-    anim.add_layer();
-
-    // Map: [-PI,PI] -> [-1,1]
-    double lastX, lastY;
-
     const auto getMapX = [outW](double x) { return outW/2 + x / M_PI * outW/2; };
     const auto getMapY = [outH](double y) { return outH/2 - y * outH/2; };
-
-    for(int i = 0; i < sections + 1; ++i) {
-        const double x = -M_PI + 2 * M_PI / sections * i;
-        const auto expect = sin(x);
-        if(i>0) {
-            const double x1 = getMapX(lastX);
-            const double y1 = getMapY(lastY);
-            const double x2 = getMapX(x);
-            const double y2 = getMapY(expect);
-            anim.frame().line(x1, y1, x2, y2);
+    {
+        HtmlAnim::Vec2Vector points;
+        for(int i = 0; i < sections + 1; ++i) {
+            const double x = -M_PI + 2 * M_PI / sections * i;
+            const auto y = sin(x);
+            points.emplace_back(HtmlAnim::Vec2(getMapX(x), getMapY(y)));
         }
-        lastX = x;
-        lastY = expect;
+        anim.frame().save()
+            .fill_style("white").rect(0, 0,
+                static_cast<double>(anim.get_width()), static_cast<double>(anim.get_height()), true)
+            .stroke_style("gray")
+            .line(0,outH/2, outW,outH/2)
+            .line(outW/2,0, outW/2,outH)
+            .stroke_style("green")
+            .line(points);
     }
-    anim.write_file("progress.html");
-    return;
+    anim.add_layer();
 
     const size_t popSize = 1000;
     const double mutationSpread = 1;
@@ -136,6 +129,7 @@ void evolution1()
     const auto start = std::chrono::high_resolution_clock::now();
 
     size_t generation = 1;
+    std::vector<double> outputs;
     while(generation < numGens) {
         const double halflifeFactor = 1.0; // TODO this returns inf (mutationHalflife > 0) ? pow(2.0, -(generation % 1000) / mutationHalflife) : 1;
         const double spread = (1.0 - (generation / static_cast<double>(numGens))) * 0.25; // TODO std::max(mutationSpread * halflifeFactor, mutationSpread * 0.01);
@@ -153,6 +147,21 @@ void evolution1()
                       << " // avg " << sum / pop.size()
                       << " // best " << pop.getIndividual(0)->getFitness()
                       << "\n";
+
+            const auto& nn = dynamic_cast<NnIndividual*>(pop.getIndividual(0))->nn;
+            HtmlAnim::Vec2Vector points;
+            for(int i = 0; i < sections + 1; ++i) {
+                const double x = -M_PI + 2 * M_PI / sections * i;
+                double inputs = x / M_PI;
+                const auto resultIdx = nn.run(&inputs, outputs);
+                const auto y = outputs[resultIdx];
+                points.emplace_back(HtmlAnim::Vec2(getMapX(x), getMapY(y)));
+            }
+            anim.frame().save()
+                .text(50, 50, std::to_string(generation))
+                .stroke_style("red")
+                .line(points);
+            anim.next_frame();
         }
 
         ++generation;
@@ -161,6 +170,8 @@ void evolution1()
     const auto stop = std::chrono::high_resolution_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     std::cout << "Time: " << duration.count() << " ms" << std::endl;
+
+    anim.write_file("progress.html");
 }
 
 int main(int argc, char **argv)
